8506d7cd8a7df59b549ed5a42c0f6a355e16c660
Mahathi Amencherla <mahathiamencherla15@gmail.com>
2021-04-08 18:03:29 +0530

    text rotator

--- my_website/src/App.js
+++ my_website/src/App.js
@@ -1,30 +1,8 @@
+import Home from './pages/home';
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div class="header">
-      <div class="inner-header">
-        <div class="header-text">
-          <h1>Welcome to Mahathi's personal website.</h1>
-          <h3>Something awesome's being constructed right now. Stay tuned!</h3>
-        </div>
-      </div>
-      <div>
-        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
-        <defs>
-        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-        </defs>
-        <g class="parallax">
-        <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
-        <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
-        <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
-        <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
-        </g>
-        </svg>
-        </div>
-    </div>
+    <Home/>
   );
 }
 
